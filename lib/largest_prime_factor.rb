d63e2dda7e092b029ac6627c58be6ea076ad33f1
onyoo <roberto9m9@gmail.com>
2015-11-02 18:50:39 +0100

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,21 @@
 # Enter your procedural solution here!
+require 'prime'
+include Math
+# puts "Please enter the number you want the largest prime factor of:"
+# target = gets.strip.to_i
+limit = 600851475143
+
+def largest_prime_factor(target = limit)
+  
+  # defines n to cut 'limit' to a managable size
+  n = (Math.sqrt(target).to_i/2)+2
+  puts "#{n}"
+  len = Math.log10(target).to_i + 1
+  n = n * len
+
+  # gathers (target/n) prime numbers
+  set = Prime.first (target/n).to_i
+
+  good_set = set.select{|primes| target%primes == 0}
+  good_set[-1]
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,26 @@
 # Enter your object-oriented solution here!
+
+require 'prime'
+include Math
+# puts "Please enter the number you want the largest prime factor of:"
+# target = gets.strip.to_i
+
+class LargestPrimeFactor
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def number(target = @limit)
+  
+    # defines n to cut 'limit' to a managable size
+    @n = (Math.sqrt(target).to_i/2)+2
+    @len = Math.log10(target).to_i + 1
+    @n = @n * @len
+
+    # gathers (target/n) prime numbers
+    @set = Prime.first (target/@n).to_i
+
+    @good_set = @set.select{|primes| target%primes == 0}
+    @good_set[-1]
+  end
+end
